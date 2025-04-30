c6f7de5c85fff943781a9bcbd1784da9357e2c50
grigory <grigory@goodnotes.com>
2025-04-30 22:11:00 +0100

    test: /private-2 is not working :-(

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,12 +1,65 @@
 package com.example
 
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
-fun Application.module() {
-    configureSecurity()
-    configureRouting()
+class CustomAuthScope {
+    fun ApplicationCall.authenticatedUser() = principal<UserIdPrincipal>() ?: throw Exception("User is not authenticated")
 }
+
+inline fun Route.requiredAuth(crossinline build: CustomAuthScope.() -> Unit) =
+    authenticate("user-header") {
+        CustomAuthScope().apply {
+            build()
+        }
+    }
+
+
+fun Application.module() {
+
+    install(Authentication) {
+        provider("user-header") {
+            authenticate { context: AuthenticationContext ->
+                val userName = context.call.request.headers["user"] ?: return@authenticate
+                context.principal(UserIdPrincipal(userName))
+            }
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+
+        authenticate("user-header") {
+            CustomAuthScope().apply {
+                get("/private") {
+                    val userName = call.authenticatedUser().name
+                    call.respondText("Hello $userName!")
+                }
+            }
+        }
+
+        requiredAuth {
+            get("/private-2") {
+                val userName = call.authenticatedUser().name
+                call.respondText("Hello to $userName from private-2!")
+            }
+        }
+
+        authenticate("user-header") {
+            get("/public") {
+                val userName = call.principal<UserIdPrincipal>()?.name ?: "unauthorized"
+                call.respondText("Hello $userName !")
+            }
+        }
+
+    }
+}
+
--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -9,43 +9,44 @@ import kotlin.test.assertEquals
 
 class ApplicationTest {
 
-    @Test
-    fun testRoot() = testApplication {
-        application { module() }
-
-
-        client.get("/").apply {
-            println("get / << "  + bodyAsText())
-            assertEquals(HttpStatusCode.OK, status)
+    @Test fun tsstRoutes() = testApplication {
+        application {
+            module()
         }
 
-
-        client.get("/private").apply {
-            println("get /private << "  + bodyAsText())
+        client.get("/").apply {
+            println(
+                """
+                    ////////////////////////////////////////////////////////////////////////////////////
+                    get / << ${bodyAsText()} 
+                """.trimIndent()
+            )
             assertEquals(HttpStatusCode.OK, status)
         }
 
         client.get("/private"){
             headers.append("user", "Test User")
         }.apply {
-            println("get /private{user=Test User} << "  + bodyAsText())
+            println(
+                """
+                    ////////////////////////////////////////////////////////////////////////////////////
+                    get /private{user=Test User} << ${bodyAsText()} 
+                """.trimIndent()
+            )
             assertEquals(HttpStatusCode.OK, status)
         }
 
-        client.get("/public").apply {
-            println("get /public << "  + bodyAsText())
-            assertEquals(HttpStatusCode.OK, status)
-        }
-
-        client.get("/public"){
+        client.get("/private-2"){
             headers.append("user", "Test User")
         }.apply {
-            println("get /public{user=Test User} << "  + bodyAsText())
+            println(
+                """
+                    ////////////////////////////////////////////////////////////////////////////////////
+                    get /private-2{user=Test User} << ${bodyAsText()} 
+                """.trimIndent()
+            )
             assertEquals(HttpStatusCode.OK, status)
         }
-
     }
-
-
 
 }
