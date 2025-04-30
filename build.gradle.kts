b6921a33ee1c671a0e01819fbad8fd3830d85128
grigory <grigory@goodnotes.com>
2025-04-30 21:46:19 +0100

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("io.ktor:ktor-server-auth")
     implementation("io.ktor:ktor-server-netty")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-cors:3.1.2")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,6 +1,7 @@
 package com.example
 
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
@@ -10,12 +11,41 @@ class ApplicationTest {
 
     @Test
     fun testRoot() = testApplication {
-        application {
-            module()
-        }
+        application { module() }
+
+
         client.get("/").apply {
+            println("get / << "  + bodyAsText())
             assertEquals(HttpStatusCode.OK, status)
         }
+
+
+        client.get("/private").apply {
+            println("get /private << "  + bodyAsText())
+            assertEquals(HttpStatusCode.OK, status)
+        }
+
+        client.get("/private"){
+            headers.append("user", "Test User")
+        }.apply {
+            println("get /private{user=Test User} << "  + bodyAsText())
+            assertEquals(HttpStatusCode.OK, status)
+        }
+
+        client.get("/public").apply {
+            println("get /public << "  + bodyAsText())
+            assertEquals(HttpStatusCode.OK, status)
+        }
+
+        client.get("/public"){
+            headers.append("user", "Test User")
+        }.apply {
+            println("get /public{user=Test User} << "  + bodyAsText())
+            assertEquals(HttpStatusCode.OK, status)
+        }
+
     }
+
+
 
 }
