bd78659b0bc177d7e24594e8c8a8274a681dbb1c
grigory <grigory@goodnotes.com>
2025-05-02 11:20:00 +0100

    fixing of the issue

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,21 +4,44 @@ import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.utils.io.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
-class CustomAuthScope {
+class CustomAuthRoute(parent: RoutingNode?, selector: RouteSelector, developmentMode: Boolean = false, environment: ApplicationEnvironment) : RoutingNode(parent, selector, developmentMode, environment) {
+
     fun ApplicationCall.authenticatedUser() = principal<UserIdPrincipal>() ?: throw Exception("User is not authenticated")
 }
 
-inline fun Route.requiredAuth(crossinline build: CustomAuthScope.() -> Unit) =
+@KtorDsl
+fun Route.requiredAuth(build: CustomAuthRoute.() -> Unit) =
     authenticate("user-header") {
-        CustomAuthScope().apply {
-            build()
+        // hack #1: the code if a child creating is copyPasted from the `RoutingNode.createChild()` method.
+        val childrenNodesMutableList = (this as RoutingNode).children as MutableList<RoutingNode>
+        val newSelector: RouteSelector = CustomAuthRouteSelector()
+
+        val existingRoute = childrenNodesMutableList.firstOrNull { it.selector == newSelector }
+        val customAuthRoute = if (existingRoute == null) {
+            // not sure if the `if` required in our case..
+            // it looks like the existingRoute is always null
+            // (newSelector is just a newly created object, and the `==` is just the references comparing in this case)
+            val newRoute = CustomAuthRoute(this, selector, developmentMode, environment)
+            childrenNodesMutableList.add(newRoute)
+            newRoute
+        } else {
+            existingRoute
         }
+
+        @Suppress("UNCHECKED_CAST")
+        customAuthRoute.apply(build as (Route.() -> Unit) ) // <- the cast is hack #2 :-)
     }
+
+class CustomAuthRouteSelector : RouteSelector() {
+    override suspend fun evaluate(context: RoutingResolveContext, segmentIndex: Int) = RouteSelectorEvaluation.Transparent
+    override fun toString() = "CustomAuthRouteSelector()"
+}
 
 
 fun Application.module() {
@@ -37,19 +60,8 @@ fun Application.module() {
             call.respondText("Hello World!")
         }
 
-        // works fine.
-        authenticate("user-header") {
-            CustomAuthScope().apply {
-                get("/private") {
-                    val user = call.authenticatedUser()
-                    call.respondText("Hello ${user.name}!")
-                }
-            }
-        }
-
-        // doesn't work :-( - the `principal<UserIdPrincipal>()` always returns `null`
         requiredAuth { // <- builds principal of a certain type
-            get("/private-2") {
+            get("/private") {
                 val user = call.authenticatedUser()  // <- returns the principal of the certain type
                 call.respondText("Hello ${user.name}!")
             }
--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -4,51 +4,41 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
+
+    val logger: Logger = LoggerFactory.getLogger(ApplicationTest::class.java)
 
     @Test fun testRoutes() = testApplication {
         application {
             module()
         }
 
-        client.get("/").apply {
-            println(
-                """
-                    |////////////////////////////////////////////////////////////////////////////////////
-                    |get / << ${bodyAsText()} 
-                    |
-                """.trimMargin()
-            )
-            assertEquals(HttpStatusCode.OK, status)
-        }
+        testRoute("/", user = null, expectedStatus = HttpStatusCode.OK)
 
-        client.get("/private"){
-            headers.append("user", "Test User")
-        }.apply {
-            println(
-                """
-                    |////////////////////////////////////////////////////////////////////////////////////
-                    |get /private{user=Test User} << ${bodyAsText()} 
-                    |
-                """.trimMargin()
-            )
-            assertEquals(HttpStatusCode.OK, status)
-        }
+        testRoute("/public", user = null,       expectedStatus = HttpStatusCode.OK)
+        testRoute("/public", user = "TestUser", expectedStatus = HttpStatusCode.OK)
 
-        client.get("/private-2"){
-            headers.append("user", "Test User")
+        testRoute("/private", user = null,       expectedStatus = HttpStatusCode.InternalServerError)
+        testRoute("/private", user = "TestUser", expectedStatus = HttpStatusCode.OK)
+    }
+
+    private suspend fun ApplicationTestBuilder.testRoute(path: String, user: String?, expectedStatus: HttpStatusCode) {
+        client.get(path){
+            if (user != null) headers.append("user", user)
         }.apply {
-            println(
+            logger.info(
                 """
                     |////////////////////////////////////////////////////////////////////////////////////
-                    |get /private-2{user=Test User} << ${bodyAsText()} 
+                    |get /path ${ if (user != null) "{user=$user}" else ""} << ${bodyAsText()} 
                     |
                 """.trimMargin()
             )
-            assertEquals(HttpStatusCode.OK, status)
+            assertEquals(expectedStatus, status)
         }
     }
 
